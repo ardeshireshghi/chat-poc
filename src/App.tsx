2946442185c7efeb3d3cf3ffbe4ef2b5ed323b5a
Ardeshir Eshghi <ardeshir.eshghi@indexlabs.co.uk>
2021-03-08 00:25:36 +0000

    Update the main `AppContainer` styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,26 @@
 import React from 'react';
-import styled, { keyframes, ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styleguide/theme';
 import { ChatScreen } from './screens/ChatScreen';
-
-const backgroundAnimation = keyframes`
-  0% {
-    background-color: yellow;
-  }
-  50% {
-    background-color: #2196f3;
-  }
-
-  100% {
-    background-color: yellow;
-  }
-`;
 
 const AppContainer = styled.div`
   display: flex;
   flex-direction: column;
-  max-width: 500px;
+  max-width: 600px;
   min-height: 100vh;
   margin: 0 auto;
   justify-content: center;
 `;
 
-function App(): JSX.Element {
+const App: React.FC = () => {
   return (
     <AppContainer>
       <ChatScreen />
     </AppContainer>
   );
-}
+};
 
-const AppWithStyles = (): JSX.Element => {
+const AppWithStyles: React.FC = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
       <App />
